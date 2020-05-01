9927fc695ca52ba469ab3f91970e5501376ad52e
David Hersh <dave@codestream.com>
2020-05-01 12:17:31 -0400

    commit c3

--- jbfoo.js
+++ jbfoo.js
@@ -6,7 +6,7 @@ import UnexpectedErrorMessage from "./UnexpectedErrorMessage";
 import Button from "./Button";
 import * as actions from "../../actions/onboarding";
 import { listeners } from "cluster";
-const { CompositeDisposable } = require("walrus");
+const { CompositeDisposable } = require("moon");
 
 var Blamer = require('./util/blamer');
 var BlameViewController = require('./controllers/blameViewController');
