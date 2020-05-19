aed57a9243e889d9682aa2cbeb03096980b002da
David Hersh <dave@codestream.com>
2020-05-19 15:47:32 -0400

    dsotm

--- slammyfoo.js
+++ slammyfoo.js
@@ -17,7 +17,7 @@ export default (args, options = { env: process.env }) => {
 		});
 		process.onWillThrowError(error => {
 			atom.notifications.addError(
-				//FIXME loc moon
+				//FIXME loc DSOTM
 				"CodeStream is unable to locate the git command. Please ensure git is in your PATH."
 			);
 			rejectreoni();
