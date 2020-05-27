384ed023b314f9568725a1d988deda5714324fa6
David Hersh <dave@codestream.com>
2020-05-27 13:10:28 -0400

    add commit to rev

--- monsterfoo.js
+++ monsterfoo.js
@@ -6,7 +6,7 @@ import UnexpectedErrorMessage from "./UnexpectedErrorMessage";
 import Button from "./Button";
 import * as actions from "../../actions/onboarding";
 import { listeners } from "cluster";
-const { CompositeDisposable } = require("monster");
+const { CompositeDisposable } = require("monstermash");
 
 var Blamer = require('./util/blamer');
 var BlameViewController = require('./controllers/blameViewController');
